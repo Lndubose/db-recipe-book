b206f39212cd35d7e8f0ca2753c50dfffed6245a
lndubose <lndubose13@gmail.com>
2018-10-11 19:38:55 -0500

    Added routes Added dishes and recipes routes based off the db

--- dishes/Routes.js
+++ dishes/Routes.js
@@ -1,44 +1,51 @@
-const express = require('express');
-const db = require('./db');
+const express = require("express");
+const db = require("./db");
 
 const router = express.Router();
 
-router.get('/api/dishes', (req, res) => {
+router.get("/api/dishes", (req, res) => {
   db.getDishes()
-  	.then(response => console.log(response))
-    .catch(err => console.log(err))
+    .then(response => res.status(200).json(response))
+    .catch(err => res.status(500).json({ error: `Server error --> ${err}` }));
 });
 
-router.get('/api/dishes/:id', (req, res) => {
+router.get("/api/dishes/:id", (req, res) => {
   const { id } = req.params;
 
   db.getDish(id)
-  	.then(response => console.log(response))
-    .catch(err => console.log(err))
+    .then(response => {
+      response
+        ? res.status(200).json(response)
+        : res.status(404).json({ error: "No dish with that id." });
+    })
+    .catch(err => res.status(500).json({ error: `Server error --> ${err}` }));
 });
 
-router.post('/api/dishes', (req, res) => {
+router.post("/api/dishes", (req, res) => {
   const newDish = req.body;
 
   db.addDish(newDish)
-  	.then(response => console.log(response))
-    .catch(err => console.log(err))
+    .then(response => res.status(201).json({ id: response[0] }))
+    .catch(err => res.status(500).json({ error: `Server error --> ${err}` }));
 });
 
 //Recipe routes
-router.get('/api/recipes', (req, res) => {
+router.get("/api/recipes", (req, res) => {
   db.getRecipes()
-  	.then(response => console.log(response))
-    .catch(err => console.log(err))
+    .then(response => res.status(200).json(response))
+    .catch(err => res.status(500).json({ error: `Server error --> ${err}` }));
 });
 
-router.post('/api/recipes', (req, res) => {
+router.post("/api/recipes", (req, res) => {
   const newRecipe = req.body;
 
   db.addRecipe(newRecipe)
-  	.then(response => console.log(response))
-    .catch(err => console.log(err))
+    .then(response => res.status(201).json({ id: response[0] }))
+    .catch(err => {
+      err.errno === 19
+        ? res.status(400).json({ error: "Name of the recipe is already used." })
+        : res.status(500).json({ error: `Server error --> ${err}` });
+    });
 });
-
 
 module.exports = router;
